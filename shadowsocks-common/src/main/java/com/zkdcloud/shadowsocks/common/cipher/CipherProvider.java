a6e1ad04a96cce0d10a18211a994728ebf34e371
zk-123 <reasuryzk123@gmail.com>
2018-08-28 22:07:35 +0800

    # 验证加密算法

--- shadowsocks-common/src/main/java/com/zkdcloud/shadowsocks/common/cipher/CipherProvider.java
+++ shadowsocks-common/src/main/java/com/zkdcloud/shadowsocks/common/cipher/CipherProvider.java
@@ -1,6 +1,6 @@
 package com.zkdcloud.shadowsocks.common.cipher;
 
-import com.zkdcloud.shadowsocks.common.cipher.stream.Aes128CfbCipher;
+import com.zkdcloud.shadowsocks.common.cipher.stream.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,22 +25,22 @@ public class CipherProvider {
 
     static {
         /* aes */
-        cipherMap.put("aes-128-ctb",Aes128CfbCipher.class);
-        cipherMap.put("aes-192-ctb",Aes128CfbCipher.class);
-        cipherMap.put("aes-256-ctb",Aes128CfbCipher.class);
+        cipherMap.put("aes-128-cfb",Aes128CfbCipher.class);
+        cipherMap.put("aes-192-cfb",Aes192CfbCipher.class);
+        cipherMap.put("aes-256-cfb",Aes256CfbCipher.class);
 
         /* camellia */
-        cipherMap.put("camellia-128-cfb",Aes128CfbCipher.class);
-        cipherMap.put("camellia-192-cfb",Aes128CfbCipher.class);
-        cipherMap.put("camellia-256-cfb",Aes128CfbCipher.class);
+        cipherMap.put("camellia-128-cfb",Camellia128CfbCipher.class);
+        cipherMap.put("camellia-192-cfb",Camellia192CfbCipher.class);
+        cipherMap.put("camellia-256-cfb",Camellia256CfbCipher.class);
 
         /* chacha20 */
-        cipherMap.put("chacha20",Aes128CfbCipher.class);
-        cipherMap.put("chacha20-ietf",Aes128CfbCipher.class);
+        cipherMap.put("chacha20",Chacha20Cipher.class);
+        cipherMap.put("chacha20-ietf",Chacha20IetfCipher.class);
 
         /*others stream cipher */
-        cipherMap.put("rc4-md5",Aes128CfbCipher.class);
-        cipherMap.put("salsa20",Aes128CfbCipher.class);
+        cipherMap.put("rc4-md5",Rc4Md5Cipher.class);
+        cipherMap.put("salsa20",Salsa20Cipher.class);
     }
 
     /**
